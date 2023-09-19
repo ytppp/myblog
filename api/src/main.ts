48007b9f81c4b8dd9391605fff8e532f263ef59b
ytppp <651003708@qq.com>
2023-09-19 18:28:41 +0800

    feat: rails api 2 nestjs api

---
+++ api/src/main.ts
@@ -0,0 +1,25 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // 接口版本化管理
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI
+  })
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  await app.listen(3000);
+}
+bootstrap();
