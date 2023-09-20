84e57f6bede90f07be0dd8bbc9d0d4b5af9e9ea3
ytppp <651003708@qq.com>
2023-09-20 10:57:40 +0800

    add: add basic config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentsController } from './contents/contents.controller';
+import { getConfig } from './utils';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig],
+    }),
+  ],
   controllers: [AppController, ContentsController],
   providers: [AppService],
 })
--- api/src/contents/contents.controller.ts
+++ api/src/contents/contents.controller.ts
@@ -1,19 +1,38 @@
 import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { BusinessException } from 'src/common/exceptions/business.exception';
 
 @Controller({
   path: 'contents',
-  version: [VERSION_NEUTRAL, '1']
+  version: [VERSION_NEUTRAL, '1'],
 })
 export class ContentsController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get()
-  getHello():string {
-    return 'hello, world'
+  getHello(): string {
+    return 'hello, world';
   }
 
   @Get('findError')
   findError() {
-    const a: any = {}
-    console.log(a.b.c)
+    const a: any = {};
+    console.log(a.b.c);
     return '123';
+  }
+
+  @Get('findBusinessError')
+  findBusinessError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('你这个参数错了');
+    }
+    return '456';
+  }
+  @Get('getTestName')
+  getTestName() {
+    return this.configService.get('TEST_VALUE').name;
   }
 }
