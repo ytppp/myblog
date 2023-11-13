99e19f7bc0f004aa854cbdda7dee34a200d9e2b4
ytppp <651003708@qq.com>
2023-11-13 09:50:21 +0800

    feat: delete nestjs api

--- api/prisma/migrations/20230927025209_add_relation_of_post_and_category/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "category_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
