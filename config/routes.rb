f03e55e12052d97b8603d6a4480c4e8d8d7110b8
ytppp <651003708@qq.com>
2024-01-25 18:24:16 +0800

    feat: dev user model test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  get "pages/index"
   root "pages#index"
 
-  resources :articles
-
+  get "/signup", to: "users#new"
   get "admin", to: 'pages#admin'
 
+  resources :users
+  resources :articles
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
+      post users_url, params: { user: { email: "user@3.com", name: "user3", password: "foobar", password_confirmation: "foobar" } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
+    patch user_url(@user), params: { user: { email: "user@4.com", name: "user4", password: "foobar", password_confirmation: "foobar" } }
     assert_redirected_to user_url(@user)
   end
 
